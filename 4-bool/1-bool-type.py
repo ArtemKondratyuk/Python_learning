f8dd33b0f16645ca77a4d4eea67d0919b8cc5f5c
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2025-03-07 17:37:34 +0200

    studied Boolean types

--- 4-bool/1-bool-type.py
+++ 4-bool/1-bool-type.py
@@ -12,4 +12,44 @@
 # y = 10
 # print(x == y)  # output: False 
 
-  
+# x = 10
+# y = 10
+
+# is_equal = x == y
+# print(is_equal)
+
+# x = 10
+# y = 10
+
+# not_equal = x != y
+# print(not_equal)    # False
+
+# x = 9
+# y = 10
+
+# not_equal = x != y
+# print(not_equal)     # True
+
+# x = 10
+# y = 9
+
+# print(x > y)     #True
+# print(x < y)     #False
+# print(x >= y)    #True
+# print(x <= y)    #False
+
+# x = 1
+# print(x < 5 and x > -2)    #True
+
+# x = 6
+# print(x < 5 and x > -2)    #False
+
+# is_tudent = True 
+# print(not is_tudent)       #False
+
+# print(bool(1))    #True
+# print(bool(0))    #Falsre
+# print(bool(-1))   #True
+# print(bool("Hello, world!"))   #True
+# print(bool(""))   #False
+
