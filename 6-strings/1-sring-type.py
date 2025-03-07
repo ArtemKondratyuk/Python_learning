250dda3e7607ab01acca350ec8549b3bebc246a8
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2025-03-07 21:07:39 +0200

    add string-type

---
+++ 6-strings/1-sring-type.py
@@ -0,0 +1,49 @@
+# My_string = "Hello world"
+# print(My_string)       # Hello world
+
+# My_string = 'Hello world'
+# print(My_string)       # Hello world
+
+
+# my_string = """
+# I'm a Frontend Focused Web Developer building and managing the Front-end of Websites
+# and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
+# I also like sharing content related to the stuff that I have learned over the years in Web Development 
+# so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin and Instagram where I
+# post useful content related to Web Development and Programming
+# I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that
+# matches my skills and experience then don't hesitate to contact me.
+# """
+# print(my_string)            # text
+
+
+# first_name = "Alice"
+# last_name = "Smith"
+# full_name = first_name + " " + last_name
+# print(full_name)                    # Alice Smith
+
+
+# first_name = "Alice"
+# last_name = "Smith"
+# full_name = first_name + " " + last_name
+
+
+# length = len(full_name)
+# print(length)                # 11
+
+
+# my_integer = 100
+# my_string = str(my_integer)
+
+# print(type(my_string))        # <class 'str'>
+
+
+# my_int = int("10")
+# print(type(my_int))         # <class 'int'>
+
+
+# my_string = input("Enter a nubmer: ")
+# print(type(my_string))       # Enter a nubmer: 10 <class 'str'>
+
+
+                               
