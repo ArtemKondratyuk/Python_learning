a7bcf4037dd140e07f8b57978180ad4435b8c1c9
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2025-03-07 18:30:35 +0200

    studied if, elif, else, add sripc year leap, add README.md

---
+++ 5-if-statement/1-if-else.py
@@ -0,0 +1,58 @@
+# if True:
+#  print("Hello, world!")    # Hello, world!
+
+# if False:
+#  print("Hello, world!")     # nothing
+
+# x = 10
+# if x > 0:
+#     print("x is positive")
+
+# x = 0
+# if x > 0:
+#     print("x is positive")
+# elif x < 0:
+#     print("x is negstive")
+# else:
+#     print("x is zero")       # x is zero
+
+# x = 1
+# if x > 0:
+#     print("x is positive")
+# elif x < 0:
+#     print("x is negstive")
+# else: 
+#     print("x is zero")      # x is positive
+
+# x = -1
+# if x > 0:
+#     print("x is positive")
+# elif x < 0:
+#     print("x is negstive")
+# else:
+#     print("x is zero")      # x is negstive
+
+# x = 10
+# if x > 0:
+#     print("x is positive")
+# elif x < 1:
+#     print("x is negstive")
+# else:
+#     print("x is zero")      # x is positive
+
+# x = 10
+# y = 20
+# if x > 0 and y > 0:
+#  print("x ang y are positive")       # x ang y are positive
+
+# massage = "Hello, world!"
+# if massage:
+#     print("massage is not empty")      # massage is not empty
+
+# massage = ""
+# if massage:
+#     print("massage is not empty")      # nothing
+
+# x = 1
+# if x:
+#     print("x is not zero")      # x is not zero
---
+++ 5-if-statement/2-script-leap-year.py
@@ -0,0 +1,22 @@
+# Скрипт на проверку год высокостный или нет
+
+# year = 2003
+
+# if year % 4 == 0 and year % 100 != 0:  
+#     print("Year is leap")
+# elif year % 400 == 0:  
+#     print("Year is leap")
+# else:
+#     print("Year is not leap")     # Year is not leap   
+
+
+# Второй вариант скрипта 
+
+# year = 2004
+
+# if not year  % 4 and year % 100:  
+#     print("Year is leap")
+# elif not year % 400:  
+#     print("Year is leap")
+# else:
+#     print("Year is not leap")    # Year is leap
