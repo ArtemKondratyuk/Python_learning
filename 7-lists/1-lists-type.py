602070674a805ee60c2b321a92e0786d390619fc
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2025-03-13 20:56:38 +0200

    add lists-type.py

---
+++ 7-lists/1-lists-type.py
@@ -0,0 +1,42 @@
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)                         # ['apple', 'banana', 'cherry']
+
+
+# my_list = list()
+# print(my_list)     #  []
+
+
+# fruits = ["apple", "banana", "cherry"]
+# print(len(fruits))                      # 3
+
+
+# my_list1 = [1, 2, 3]
+# my_list2 = [1, 3, 2]
+# my_list3 = [1, 2, 3]
+
+# print(my_list1 == my_list2)     #   False
+
+
+# my_list1 = [1, 2, 3]
+# my_list2 = [1, 3, 2]
+# my_list3 = [1, 2, 3]
+
+# print(my_list1 == my_list3)    #   True
+
+
+
+# print(bool())     # False
+# print(bool([0]))  # True
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+
+# print("apple" in fruits)       # True
+# print("watermelon" in fruits)  # False
+
+
+# states = ["New York", "Montana", "Texas"]
+
+# print("Texas" in states)    #  True
+# print("Utah" in states)     #  False
