21f5bf79c876453c0d680739dfb4ad2ef1f9694b
ArtemKondratyuk <118299767+ArtemKondratyuk@users.noreply.github.com>
2025-03-18 16:35:35 +0200

    add indexes.py

---
+++ 8-indexes-slises/1-indexes.py
@@ -0,0 +1,55 @@
+# flat_number = 19
+# entrance_number = flat_number // 20
+# floor_number = flat_number % 20 // 4
+
+# print("Entrance_number: ", entrance_number)
+# print("Floor number: ", floor_number)      #   Entrance_number:  0   Floor number:  4
+
+
+# flat_number = 80
+# entrance_number = flat_number // 20
+# floor_number = flat_number % 20 // 4
+
+# print("Entrance_number: ", entrance_number)
+# print("Floor number: ", floor_number)        #  Entrance_number:  4   Floor number:  0
+
+
+
+# fruits = ["apple", "banana", "cherry", "watermelon"]
+
+# print(fruits[0])
+# print(fruits[-4])       # apple   aplle 
+
+
+
+# fruits = ["apple", "banana", "cherry", "watermelon"]
+
+# print(fruits[4])        # Error
+
+# print(fruits[-5])       # Error
+
+
+
+# fruits = ["apple", "banana", "cherry", "watermelon"]
+# fruits[0] = "penapple"
+
+# print(fruits)       #   ['penapple', 'banana', 'cherry', 'watermelon'] 
+
+
+# cars = ["Ferrari", "Audi", "Volkswagen"]
+# cars[1] = "BMW"
+
+# print(cars)      #  ['Ferrari', 'BMW', 'Volkswagen']
+
+
+# names = ["Sten", "Jordan", "Jefro", "Ted"]
+# names[3] = "Scot"
+
+# print(names)      #  ['Sten', 'Jordan', 'Jefro', 'Scot']
+
+
+# names = ["Sten", "Jordan", "Jefro", "Ted"]
+# names[1], names[3] = names[3], names[1]
+
+# print(names)    # ['Sten', 'Ted', 'Jefro', 'Jordan']
+
